16cef47b9510e50e6c5c84498781c9c2fadc792e
23navi <navisureka23@gmail.com>
2025-07-04 09:35:08 +0530

    add annotations to validate_mode

--- Basics/COPY/validation_mode.sql
+++ Basics/COPY/validation_mode.sql
@@ -17,6 +17,13 @@ CREATE OR REPLACE STAGE COPY_DB.PUBLIC.aws_stage_copy
     url='s3://snowflakebucket-copyoption/size/';
   
 LIST @COPY_DB.PUBLIC.aws_stage_copy;
+
+-- This bucket will have 2 files with size 
+/*
+s3://snowflakebucket-copyoption/size/Orders.csv : 54600
+s3://snowflakebucket-copyoption/size/Orders2.csv: 54598
+*/
+
   
     
 -- Load data using copy command
@@ -25,14 +32,18 @@ COPY INTO COPY_DB.PUBLIC.ORDERS
     file_format= (type = csv field_delimiter=',' skip_header=1)
     pattern='.*Order.*'
     VALIDATION_MODE = RETURN_ERRORS;
+
+-- The above command returns: Query produced no results (ie there were no error in loading the files) but no data will be loaded to the table.
     
-SELECT * FROM ORDERS;    
+SELECT * FROM ORDERS;     -- Verify that no data was loaded in the table when using VALIDATION_MODE
     
 COPY INTO COPY_DB.PUBLIC.ORDERS
     FROM @aws_stage_copy
     file_format= (type = csv field_delimiter=',' skip_header=1)
     pattern='.*Order.*'
    VALIDATION_MODE = RETURN_5_ROWS ;
+
+-- The above query will give the first 5 rows 
 
 
 
@@ -43,12 +54,33 @@ create or replace stage copy_db.public.aws_stage_copy
     
 list @copy_db.public.aws_stage_copy;
 
+
+/*
+We get 4 files
+
+s3://snowflakebucket-copyoption/returnfailed/OrderDetails_error.csv	54622
+s3://snowflakebucket-copyoption/returnfailed/OrderDetails_error2 - Copy.csv	10514
+s3://snowflakebucket-copyoption/returnfailed/Orders.csv	54597
+s3://snowflakebucket-copyoption/returnfailed/Orders2.csv	54598
+*/
+
 -- show all errors --
 copy into copy_db.public.orders
     from @copy_db.public.aws_stage_copy
     file_format = (type=csv field_delimiter=',' skip_header=1)
     pattern='.*Order.*'
     validation_mode=return_errors;
+
+
+/*
+Error	                	                	        File with error
+Numeric value '7-' is not recognized	                returnfailed/OrderDetails_error2 - Copy.csv	2
+Numeric value '3a' is not recognized	                returnfailed/OrderDetails_error2 - Copy.csv	4
+Numeric value 'one thousand' is not recognized	        returnfailed/OrderDetails_error.csv	2
+Numeric value 'two hundred twenty' is not recognized	returnfailed/OrderDetails_error.csv	3
+
+The above command will give me the errors and the file+line+other details about that error
+*/
 
 -- validate first n rows --
 copy into copy_db.public.orders
@@ -58,3 +90,8 @@ copy into copy_db.public.orders
     validation_mode=return_1_rows;
     
     
+/*
+The above command filed to return the first line, that means that there was an error on the first line of the first file
+
+Numeric value 'one thousand' is not recognized	: returnfailed/OrderDetails_error.csv :	2
+*/
